05fe2229de51b53962af8bca04ba66ef83cd312c
alianlbj23 <alianlbj23@gmail.com>
2024-12-29 13:17:40 +0800

    Add yolo simple pkg

---
+++ src/yolo_example_pkg/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'yolo_example_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'models'), glob('models/*.pt')),
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='alianlbj23@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'yolo_node = yolo_example_pkg.yolo_test:main',
+        ],
+    },
+)
